cf9dc0e606f630e29e0f8b54f04b66bc7954c922
faiverht <40344999+faiverht@users.noreply.github.com>
2023-05-30 19:44:47 -0500

    Corregido mayo
    Actualizado con lectura en ppm

--- Arduino/Code pruebas/LCD_03_MQ4_ppm/LCD_03_MQ4_ppm.ino
+++ Arduino/Code pruebas/LCD_03_MQ4_ppm/LCD_03_MQ4_ppm.ino
@@ -1,13 +1,20 @@
+// programa para calcular el valor de R0
 #include <Wire.h>
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
 
 #define I2C_ADDR    0x27  // Dirección del dispositivo I2C (LCD)
-#define A0_PIN A0
+#define MQ4_PIN A0
 #define D0_PIN 8
 
 int threshold;
-int A0_Out;
+int MQ4_Out;
+const float RL = 1.0; // 1K Ohm de la tarjerta MQ4
+const float CLEAN_AIR_RS_RO = 4.3;  // Rs/Ro según datasheet
+float MQ4_volt;
+float RS_gas;     // Valor de Rs en un gas
+float ratio;      // Rs_Gas/Rs_Aire
+float RO;         // valor de RO
 
 LiquidCrystal_I2C             lcd(I2C_ADDR,2, 1, 0, 4, 5, 6, 7);
 void setup()
@@ -17,27 +24,34 @@ void setup()
        lcd.setBacklightPin(3,POSITIVE);
        lcd.setBacklight(HIGH);
 
-       lcd.home ();                   // go home
-       lcd.print("   Prueba LCD");
+       lcd.home();                   // go home
+       lcd.print("Test MQ4: ");
        lcd.setCursor( 0, 1 );        // go to the 2nd line
        lcd.print("Faiver H T G. Ok");
-       delay(2000);
-       lcd.setCursor( 0, 1 );
-       lcd.print(" MQ4 =          ");
+
    }
 void loop()
    {
-    A0_Out = analogRead(A0_PIN);  // lee el canal análogo desde el sensor MQ4
-    
-    lcd.setCursor( 7, 1 );        // go to (x,y) en LCD
-    //lcd.print(lectura_ADC, "  ");
-    //sprintf();
+    lcd.setCursor( 10, 0 );        // go to (x,y) en LCD
+    lcd.print("V=");
 
-    //lcd.printf(" %d  ",lectura_ADC);
-    lcd.print("    ");
-    lcd.setCursor( 7, 1 );
-    lcd.print(A0_Out);
+    MQ4_Out = analogRead(MQ4_PIN);  // lee el canal análogo desde el sensor MQ4
+    MQ4_volt = (float)MQ4_Out/1024*5.0;
+    RS_gas = (5.0-MQ4_volt)/MQ4_volt; // se omite RL = 1k
+    //RO = RS_gas/CLEAN_AIR_RS_RO;   // RO
+    //ratio = RS_gas/R0
+    ratio = RS_gas/5.18;
 
+    // se imprime este valor para luego ser utilizada en la siguiente:
+
+    lcd.print(MQ4_volt);
+    lcd.setCursor(0, 1 );
+    lcd.print("/=");  // Ratio
+    lcd.print(RS_gas,1);
+    // Ro: R del sensor en una concentración conocida (p ej aire ambiente)
+
+
+    lcd.print(" R/R0=");
+    lcd.print(ratio);
     delay(500);
-
    }
